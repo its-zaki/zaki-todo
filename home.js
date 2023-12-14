a5b9b5d461a2d2f475864339e1722b1bf413b514
its zaki <zakiyounus789@gmail.com>
2023-12-14 16:34:21 +0500

    crud done

--- home.js
+++ home.js
@@ -11,6 +11,7 @@ import {
   doc,
   deleteDoc,
   updateDoc,
+  query,orderBy,where
 } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-firestore.js";
 
 // getting html tags
@@ -18,13 +19,22 @@ import {
 const form = document.querySelector("form");
 const title = document.querySelector("#title");
 const card = document.querySelector("#card");
+const username = document.querySelector("#username");
 
 //user login or logout function
-
-onAuthStateChanged(auth, (user) => {
+let userid;
+onAuthStateChanged(auth, async(user) => {
   if (user) {
+    userid = user.uid
     const uid = user.uid;
     console.log(uid);
+    render(userid)
+    const querySnapshot = await getDocs(collection(db,"users"));
+    querySnapshot.forEach((doc) => {
+      // console.log(doc.data());
+      username.innerHTML = `${doc.data().names}'s Todo`;
+    });
+
   } else {
     window.location = "index.html";
   }
@@ -46,7 +56,22 @@ logout.addEventListener("click", () => {
 
 //getting user data from firestore function
 let arr = [];
-function render() {
+async function render(userid) {
+  card.innerHTML = ""
+  arr.length = 0
+  const q = query(
+    collection(db, "posts"),
+    where("uid" ,"==", userid),
+    orderBy("postDate", "desc")
+
+  );
+  const querySnapshot = await getDocs(q);
+  querySnapshot.forEach((doc) => {
+    arr.push({ ...doc.data(), docId: doc.id });
+    // console.log(doc.id);
+  });
+  console.log(arr);
+
   arr.map((item) => {
     card.innerHTML += `
     <div class="card mt-3">
@@ -70,8 +95,8 @@ function render() {
       await deleteDoc(doc(db, "posts", arr[index].docId)).then(() => {
         console.log("post deleted");
         arr.splice(index, 1);
-        render();
       });
+      render(userid);
     });
   });
   edited.forEach((btn, index) => {
@@ -82,68 +107,22 @@ function render() {
         title: updatedTitle,
       });
       arr[index].title = updatedTitle;
-      render();
+      render(userid);
     });
   });
 }
 
-async function getDataFromFirestore() {
-  const querySnapshot = await getDocs(collection(db, "posts"));
-  querySnapshot.forEach((doc) => {
-    arr.push({ ...doc.data(), docId: doc.id });
-    // console.log(doc.id);
-  });
-  console.log(arr);
-  render();
-  // arr.map((item) => {
-  //   card.innerHTML += `
-  //   <div class="card mt-3">
-  //   <div class="card-body ">
-  //       <div class="buttons-div">
-  //       <div>
-  //       <p><span class="h4">${item.title}</span></p>
-  //       </div>
-  //           <div>
-  //               <button class="del" ><i class="ri-delete-bin-line"></i></button><button class="edit"><i class="ri-pencil-fill"></i></button>
-  //           </div>
-  //       </div>
-  //   </div>
-  // </div>`;
-  //   // console.log(item);
-  // });
-  // const deleted = document.querySelectorAll(".del")
-  // const edited = document.querySelectorAll(".edit")
-  // deleted.forEach((btn, index)=>{
-  //   btn.addEventListener('click',async (
-  //   )=>{
-  //     console.log("delete call", index);
-  //     await deleteDoc(doc(db, "posts", arr[index].docId))
-  //     .then(() => {
-  //       console.log('post deleted');
-  //       arr.splice(index, 1);
-  //       render()
-  //   });
-
-  //   })
-  // })
-  // edited.forEach((btn, index)=>{
-  //   btn.addEventListener('click', async()=>{
-  //     console.log("edit call" , index);
-  //     const updatedTitle = prompt('enter new Title');
-  //           await updateDoc(doc(db, "posts", arr[index].docId), {
-  //               title: updatedTitle
-  //           });
-  //           arr[index].title = updatedTitle;
-  //           render()
-  //   })
-  // })
-}
-getDataFromFirestore()
-// render()
-//add data in firestore function
 
 form.addEventListener("submit", async (event) => {
   event.preventDefault();
+  if(title.value === ""){
+    Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: "Enter Any Todo",
+    });
+    return
+  }
   console.log(title.value);
   card.innerHTML = "";
   try {
@@ -153,7 +132,14 @@ form.addEventListener("submit", async (event) => {
       postDate: Timestamp.fromDate(new Date()),
     });
     console.log("Document written with ID: ", docRef.id);
-    render()
+    Swal.fire({
+      position: "top-center",
+      icon: "success",
+      title: "Todo Successfully Added",
+      showConfirmButton: false,
+      timer: 500
+    });
+    render(userid)
   } catch (e) {
     console.error("Error adding document: ", e);
   }
--- register.js
+++ register.js
@@ -1,33 +1,44 @@
 import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js";
-import { auth } from "./config.js";
+import { auth ,db} from "./config.js";
+import {
+  collection,
+  addDoc
+} from "https://www.gstatic.com/firebasejs/9.10.0/firebase-firestore.js";
+const form = document.querySelector("#form");
+const email = document.querySelector("#email");
+const password = document.querySelector("#pass");
+const name = document.querySelector("#name");
 
-
-const form = document.querySelector("#form")
-const email = document.querySelector("#email")
-const password = document.querySelector("#pass")
-const name = document.querySelector("#name")
-
-
-form.addEventListener("submit", (event)=>{
-    event.preventDefault();
-    createUserWithEmailAndPassword(auth, email.value, password.value, name.value)
-  .then((userCredential) => {
-    const user = userCredential.user;
-    console.log(user);
-    name.value = ''
-    email.value = ''
-    password.value = ''
-    window.location ="index.html"
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    console.log(errorMessage);
-    Swal.fire({
-      icon: "error",
-      title: "Oops...",
-      text: errorMessage,
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  createUserWithEmailAndPassword(auth, email.value, password.value)
+    .then((userCredential) => {
+      const user = userCredential.user;
+      console.log(user);
+      const names = name.value;
+      addDoc(collection(db, "users"), {
+        names: names,
+        email: email.value,
+      })
+        .then((res) => {
+          console.log(res);
+          window.location = "home.html";
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+      name.value = "";
+      email.value = "";
+      password.value = "";
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorMessage);
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: errorMessage,
+      });
     });
-    
-  });
-})
+});
